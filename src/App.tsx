d95c2c6a069f36b40c00b04cbb382536712aa091
Ana-Ma-Web <90105049+Ana-Ma-Web@users.noreply.github.com>
2023-07-30 01:21:46 +0300

    chore: add mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
 import { type ReactElement } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { Box, Container, Link, Typography } from "@mui/material";
+import ProTip from "./ProTip";
+
+function Copyright(): ReactElement {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}.
+    </Typography>
+  );
+}
 
 function App(): ReactElement {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Material UI Create React App example in TypeScript
+        </Typography>
+        <ProTip />
+        <Copyright />
+      </Box>
+    </Container>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "@fontsource/playfair-display/400.css";
+import "@fontsource/bellota-text/400.css";
+import "@fontsource/economica/400.css";
+import theme from "./theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
 
