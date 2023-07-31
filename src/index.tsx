cf1585c02dbf15a5404274b4b7a68fb5bdd228c4
Ana-Ma-Web <90105049+Ana-Ma-Web@users.noreply.github.com>
2023-08-01 00:41:33 +0300

    chore: add react router and redux toolkit

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -7,16 +8,19 @@ import "@fontsource/playfair-display/400.css";
 import "@fontsource/bellota-text/400.css";
 import "@fontsource/economica/400.css";
 import theme from "./theme";
+import { store } from "./store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
 );
 
