9727048c8a10f7ca9625074fd9c9d40766f837db
igorgromykojs <125064212+igorgromykojs@users.noreply.github.com>
2023-08-05 15:20:17 +0300

    feat: make all fields

--- src/components/register/Register.tsx
+++ src/components/register/Register.tsx
@@ -1,9 +1,93 @@
-import React from 'react';
 import { type ReactElement } from 'react';
-import { Box } from '@mui/material';
+import {
+  Button,
+  TextField,
+  Typography,
+  InputLabel,
+  Autocomplete,
+} from '@mui/material';
 
 function RegisterForm(): ReactElement {
-  return <Box textAlign={'center'}>Register Page</Box>;
+  const contries = ['U.S.', 'France', 'Canada'];
+  return (
+    <div className="root">
+      <Typography variant="h2" textAlign={'center'}>
+        Welcome
+      </Typography>
+      <Typography variant="body1" textAlign={'center'}>
+        Register your account
+      </Typography>
+      <TextField
+        fullWidth={true}
+        margin="normal"
+        label="Email"
+        variant="outlined"
+        placeholder="Enter your email"
+      />
+      <TextField
+        fullWidth={true}
+        margin="normal"
+        label="Password"
+        variant="outlined"
+        placeholder="Enter your password"
+      />
+      <TextField
+        fullWidth={true}
+        margin="normal"
+        label="First name"
+        variant="outlined"
+        placeholder="Enter your first name"
+      />
+      <TextField
+        fullWidth={true}
+        margin="normal"
+        label="Last name"
+        variant="outlined"
+        placeholder="Enter your last name"
+      />
+      <input type="date" />
+      <InputLabel id="country-select-label">Select Country</InputLabel>
+      <TextField
+        fullWidth={true}
+        margin="normal"
+        label="Street"
+        variant="outlined"
+        placeholder="Enter your street"
+      />
+      <TextField
+        fullWidth={true}
+        margin="normal"
+        label="City"
+        variant="outlined"
+        placeholder="Enter your city"
+      />
+      <TextField
+        fullWidth={true}
+        margin="normal"
+        label="Postal code"
+        variant="outlined"
+        placeholder="Enter your street"
+      />
+      <Autocomplete
+        disablePortal
+        id="combo-box-demo"
+        options={contries}
+        sx={{ width: 300 }}
+        renderInput={(params) => <TextField {...params} label="Country" />}
+      />
+      <Button type="submit" variant="contained">
+        Register
+      </Button>
+      <Typography variant="body1">
+        Dont have any acount?
+        <span
+          style={{ color: '#1900D5', marginLeft: '10px', cursor: 'pointer' }}
+        >
+          Sign In
+        </span>
+      </Typography>
+    </div>
+  );
 }
 
 export default RegisterForm;
