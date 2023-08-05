ba2f80b289235f3300f44b9a9b6a1daf501f7621
Ana-Ma-Web <90105049+Ana-Ma-Web@users.noreply.github.com>
2023-08-05 13:30:37 +0300

    feat: add client api calls

---
+++ src/api/calls/authCustomer.ts
@@ -0,0 +1,28 @@
+import {
+  type ClientResponse,
+  type CustomerSignInResult,
+} from '@commercetools/platform-sdk';
+import { apiRoot } from '../client/BuildClient';
+
+// Пользователь найден: statusCode: 200
+// Неправильный email или пароль: statusCode: 400
+export const authCustomer = async (props: {
+  email: string;
+  password: string;
+}): Promise<ClientResponse<CustomerSignInResult>> => {
+  return apiRoot
+    .login()
+    .post({
+      body: {
+        email: props.email,
+        password: props.password,
+      },
+    })
+    .execute();
+};
+
+// Пример использования:
+
+// authCustomer({ email: 'example-email.example.com', password: 'password' })
+//   .then(console.log)
+//   .catch(console.error);
---
+++ src/api/calls/createCustomer.ts
@@ -0,0 +1,28 @@
+import {
+  type ClientResponse,
+  type CustomerSignInResult,
+} from '@commercetools/platform-sdk';
+import { apiRoot } from '../client/BuildClient';
+
+// Пользователь создан: statusCode: 201
+// Пользователь уже существует: statusCode: 400
+export const createCustomer = async (props: {
+  email: string;
+  password: string;
+}): Promise<ClientResponse<CustomerSignInResult>> => {
+  return apiRoot
+    .customers()
+    .post({
+      body: {
+        email: props.email,
+        password: props.password,
+      },
+    })
+    .execute();
+};
+
+// Пример использования:
+
+// createCustomer({ email: 'example-email.example.com', password: 'password' })
+//   .then(console.log)
+//   .catch(console.error);
