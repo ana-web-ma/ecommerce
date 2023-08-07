98ce9a4216904f46ce1d4edf0870f142ac8f4af4
VitalyRK <shishovip@yandex.ru>
2023-08-07 10:47:17 +0300

    feat: (ECOM-2_19) implement routing for navigation between pages (Mon, Aug  7, 2023 10:47:17 AM)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import { type ReactElement } from 'react';
-import { Container } from '@mui/material';
+import { Container, CssBaseline, ThemeProvider } from '@mui/material';
+import { Routes, Route } from 'react-router-dom';
 import RegisterPage from './pages/register/RegisterPage';
+import LoginPage from './pages/login/LoginPage';
+import MainPage from './pages/main/MainPage';
+import theme from './theme';
 
 function App(): ReactElement {
   return (
-    <Container maxWidth="xl">
-      <RegisterPage></RegisterPage>
-    </Container>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container maxWidth="xl">
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </Container>
+    </ThemeProvider>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { CssBaseline, ThemeProvider } from '@mui/material';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '@fontsource/playfair-display/400.css';
 import '@fontsource/bellota-text/400.css';
 import '@fontsource/economica/400.css';
-import theme from './theme';
 import { store } from './store';
 
 const root = ReactDOM.createRoot(
@@ -16,10 +15,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
+      <BrowserRouter>
         <App />
-      </ThemeProvider>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
 );
