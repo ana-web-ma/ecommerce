449c70a2d09b64d36fa59734b6b148366df36bd0
VitalyRK <shishovip@yandex.ru>
2023-08-08 12:39:30 +0300

    feat:(ECOM-2_19)implement routing for navigation between login, registration, and main pages (Tue, Aug  8, 2023 12:39:30 PM)

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,46 @@ import LoginPage from './pages/login/LoginPage';
 import MainPage from './pages/main/MainPage';
 import theme from './theme';
 
+const useHash = (): [string, (newHash: string) => void] => {
+  // Tracks the browser's location hash value, and allows changing it.
+  // https://www.30secondsofcode.org/react/s/use-hash/
+  const [hash, setHash] = React.useState(() => window.location.hash);
+
+  const hashChangeHandler = React.useCallback(() => {
+    setHash(window.location.hash);
+  }, []);
+
+  React.useEffect(() => {
+    window.addEventListener('hashchange', hashChangeHandler);
+    return () => {
+      window.removeEventListener('hashchange', hashChangeHandler);
+    };
+  }, []);
+
+  const updateHash = React.useCallback(
+    (newHash: string) => {
+      if (newHash !== hash) window.location.hash = newHash;
+    },
+    [hash],
+  );
+
+  return [hash, updateHash];
+};
+
 function App(): ReactElement {
+  const [hash, setHash] = useHash();
+  React.useEffect(() => {
+    setHash('#/parfume');
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Container maxWidth="xl">
         <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/parfume" element={<MainPage />} />
+          <Route path="/parfume/login" element={<LoginPage />} />
+          <Route path="/parfume/register" element={<RegisterPage />} />
         </Routes>
       </Container>
     </ThemeProvider>
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '@fontsource/playfair-display/400.css';
@@ -15,9 +15,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
 );
