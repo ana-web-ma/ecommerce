d5b6bdb5a92c0873c68608175fdeb3228e24d9c0
VitalyRK <shishovip@yandex.ru>
2023-08-08 19:21:16 +0300

    feat:(ECOM-2_20) add not found page (Tue, Aug  8, 2023  7:21:16 PM)

---
+++ src/pages/not-found/NotFound.tsx
@@ -0,0 +1,60 @@
+import React, { type ReactElement } from 'react';
+import { Box, Button, Container, Grid, Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+
+const NotFound = (): ReactElement => {
+  const navigate = useNavigate();
+
+  return (
+    <Box sx={{ my: 2 }}>
+      <Container maxWidth="md">
+        <Grid container spacing={2} alignItems="center" justifyContent="center">
+          <Grid item xs={12} md={6}>
+            <Container
+              sx={{
+                width: '320px',
+              }}
+            >
+              <img
+                src="https://static.semrush.com/blog/uploads/files/7a/c4/7ac4acca6898c1bb4781b64dd751a8df/what-does-error-404-not-found-mean.svg"
+                alt=""
+                width={320}
+                height={300}
+              />
+            </Container>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Container
+              sx={{
+                width: '320px',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'space-between',
+                alignItems: 'end',
+              }}
+            >
+              <Typography variant="h1">404</Typography>
+              <Typography variant="h6">Page Not Found</Typography>
+              <Typography variant="subtitle2">
+                This page doesn&apos;t exist or was removed
+              </Typography>
+              <Typography variant="subtitle1">
+                We suggest you back to home
+              </Typography>
+              <Button
+                variant="contained"
+                onClick={(): void => {
+                  navigate('/parfume');
+                }}
+              >
+                Back to Home
+              </Button>
+            </Container>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
+};
+
+export default NotFound;
