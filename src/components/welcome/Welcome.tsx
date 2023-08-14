92caf8aa24586df363da2de03c2748fd11c2de9f
VitalyRK <shishovip@yandex.ru>
2023-08-14 10:55:32 +0300

    feat: (ECOM-2_05) clear code (Mon, Aug 14, 2023 10:55:32 AM)

--- src/components/welcome/Welcome.tsx
+++ src/components/welcome/Welcome.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
 import { type ReactElement } from 'react';
 import { Box, Typography } from '@mui/material';
-import { useAppSelector } from '../../helpers/hooks/Hooks';
 
 function Welcome(): ReactElement {
   return (
     <Box textAlign={'center'}>
       <Typography variant="h1">Welcome Page</Typography>
-      <Typography variant="h4">
-        {useAppSelector((state) => state.customerReducer.customer.id)}
-      </Typography>
-      <Typography variant="h4">
-        {useAppSelector((state) => state.customerReducer.isLogged).toString()}
-      </Typography>
     </Box>
   );
 }
