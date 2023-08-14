a26a23a30bcd702b36ef67a56c871312f78f3afd
VitalyRK <shishovip@yandex.ru>
2023-08-14 19:39:04 +0300

    feat: (ECOM-2_21) create header links to main login and register pages (Mon, Aug 14, 2023  7:39:04 PM)

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { type ReactElement } from 'react';
-import Link from '@mui/material/Link';
 import { IconButton, Stack } from '@mui/material';
+import { Link } from 'react-router-dom';
+import LogoutIcon from '@mui/icons-material/Logout';
+import LoginIcon from '@mui/icons-material/Login';
+import HomeIcon from '@mui/icons-material/Home';
+import AddIcon from '@mui/icons-material/Add';
 import SearchIcon from '../ui/icons/SearchIcon';
 import HeaderLink from './HeaderLink';
 import logo from './img/logo.png';
@@ -9,17 +13,20 @@ import logo from './img/logo.png';
 export default function Header(): ReactElement {
   return (
     <>
-      <Stack direction="row" justifyContent="space-between">
-        <Link>
+      <Stack direction="row" width="100%" justifyContent="space-between">
+        <IconButton component={Link} to="/">
           <img src={logo} />
-        </Link>
+        </IconButton>
         <Stack
           direction="row"
           justifyContent="center"
           alignItems="center"
           spacing={2}
         >
-          <HeaderLink text="Header link"></HeaderLink>
+          <HeaderLink text="Home" path="/" icon={<HomeIcon />} />
+          <HeaderLink text="Log in" path="/login" icon={<LoginIcon />} />
+          <HeaderLink text="Registration" path="/register" icon={<AddIcon />} />
+          <HeaderLink text="Log out" path="/" icon={<LogoutIcon />} />
         </Stack>
         <IconButton aria-label="search">
           <SearchIcon />
--- src/components/header/HeaderLink.tsx
+++ src/components/header/HeaderLink.tsx
@@ -2,15 +2,31 @@ import React from 'react';
 import { type ReactElement } from 'react';
 import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
+import { useNavigate } from 'react-router-dom';
 
-export default function HeaderLink(props: { text: string }): ReactElement {
+export default function HeaderLink(props: {
+  text: string;
+  path: string;
+  icon: ReactElement;
+}): ReactElement {
+  const navigate = useNavigate();
+
   return (
-    <Typography
-      fontFamily={'bellota-text'}
-      fontSize={13}
-      textTransform={'uppercase'}
-    >
-      <Link href="#">{props.text}</Link>{' '}
+    <Typography>
+      <Link
+        onClick={(): void => {
+          navigate(props.path);
+        }}
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          gap: '5px',
+        }}
+      >
+        {props.icon}
+        {props.text}
+      </Link>
     </Typography>
   );
 }
