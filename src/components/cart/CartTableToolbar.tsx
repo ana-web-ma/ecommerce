799153cbccc53b586ef557e36bd951998426a42d
Ana-Ma-Web <90105049+Ana-Ma-Web@users.noreply.github.com>
2023-09-18 03:56:34 +0300

    fix: fix full total price in cart

--- src/components/cart/CartTableToolbar.tsx
+++ src/components/cart/CartTableToolbar.tsx
@@ -12,7 +12,7 @@ import {
   DialogActions,
   TextField,
 } from '@mui/material';
-import React, { type ReactElement } from 'react';
+import React, { useEffect, type ReactElement } from 'react';
 import { updateCartById } from '../../api/calls/carts/updateCartById';
 import {
   useAppDispatch,
@@ -35,6 +35,17 @@ export default function CartTableToolbar(props: {
   const versionActiveCart = useVersionCart();
   const [open, setOpen] = React.useState(false);
   const [promoCodeInputValue, setPromoCodeInputValue] = React.useState('');
+  const [fullTotalPrice, setFullTotalPrice] = React.useState(0);
+
+  useEffect(() => {
+    setFullTotalPrice(
+      props.lineItems.reduce(
+        (acc, el) => acc + el.price.value.centAmount * el.quantity,
+        0,
+      ) / 100,
+    );
+    console.log('oikjniuj');
+  }, [props.totalPrice]);
 
   const handleClickOpen = (): void => {
     setOpen(true);
@@ -82,6 +93,7 @@ export default function CartTableToolbar(props: {
       .catch((err) => {
         console.log(err);
       });
+    setPromoCodeInputValue('');
   };
 
   return props.totalPrice !== undefined ? (
@@ -121,10 +133,31 @@ export default function CartTableToolbar(props: {
           justifyContent="flex-end"
           mr={3}
         >
-          <Typography variant="subtitle2">Total:</Typography>
-          <Typography variant="subtitle2">
-            {props.totalPrice.centAmount / 100}€
-          </Typography>
+          {props.totalPrice.centAmount / 100 === fullTotalPrice ? (
+            <>
+              <Typography variant="subtitle2">Total:</Typography>
+              <Typography variant="subtitle2">
+                {props.totalPrice.centAmount / 100}€
+              </Typography>
+            </>
+          ) : (
+            <Stack flexDirection={'row'} columnGap={1}>
+              <Typography variant="subtitle2" sx={{ whiteSpace: 'nowrap' }}>
+                {`${props.totalPrice.centAmount / 100} €`}
+              </Typography>
+              <Typography
+                variant="subtitle2"
+                style={{
+                  textDecorationLine: 'line-through',
+                  opacity: '0.6',
+                  fontWeight: 400,
+                  whiteSpace: 'nowrap',
+                }}
+              >
+                {`${fullTotalPrice} €`}
+              </Typography>
+            </Stack>
+          )}
         </Stack>
         <Stack mr="auto" direction="row" alignItems="center" columnGap={1}>
           <Typography variant="subtitle2">Promo code:</Typography>
